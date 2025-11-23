28394b5314034bacb13f053e97a261722ce96f85
Maksym Shevchuk <Mr.Maks@i.ua>
2025-11-24 01:08:09 +0200

    test

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import {Header} from "./components/Header/Heared.tsx";
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <main className="App"></main>
+    </>
+  )
+}
+
+export default App
